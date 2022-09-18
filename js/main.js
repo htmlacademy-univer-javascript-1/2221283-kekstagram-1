33438747795edb788c51b69610d9d7e727855ffa
Vlada <vlada.makarova.09@mail.ru>
2022-09-18 21:13:02 +0500

    add 2 functions

---
+++ js/main.js
@@ -0,0 +1,11 @@
+const randomiseNumber = (minNumber, maxNumber) => {
+  if (minNumber < 0 || minNumber >= maxNumber) {
+    return;
+  }
+  return Math.floor(Math.random() * (maxNumber - minNumber + 1) + minNumber);
+}
+
+const checkLength = (string, maxLength) => {
+   return string.length > maxLength ? false : true;
+}
+
