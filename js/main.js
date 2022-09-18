63be07f4d467f86f989baf9b8898e4d63690dd23
Vlada <vlada.makarova.09@mail.ru>
2022-09-18 22:01:17 +0500

    task 2

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 const randomiseNumber = (minNumber, maxNumber) => {
-  if (minNumber < 0 || minNumber >= maxNumber) {
+  if (minNumber < 0 || maxNumber < 0 || minNumber >= maxNumber) {
     return;
   }
   return Math.floor(Math.random() * (maxNumber - minNumber + 1) + minNumber);
 }
 
 const checkLength = (string, maxLength) => {
-   return string.length > maxLength ? false : true;
+   return string.length <= maxLength;
 }
 
