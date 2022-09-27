3a25cf8b865ad1e5f2597ea7c9f7d68a54c5186b
Vlada <vlada.makarova.09@mail.ru>
2022-09-27 23:47:09 +0500

    task 4.15

--- js/main.js
+++ js/main.js
@@ -1,3 +1,18 @@
+const COUNT = 25;
+
+const NAMES = [
+  'Ваня',
+  'Артём',
+  'Анна',
+  'Саша',
+  'Владислав'
+];
+
+const Like = {
+  MIN: 15,
+  MAX: 200
+};
+
 const randomiseNumber = (minNumber, maxNumber) => {
   if (minNumber < 0 || maxNumber < 0) {
     return -1;
@@ -10,8 +25,25 @@ const randomiseNumber = (minNumber, maxNumber) => {
   return Math.floor(Math.random() * (maxNumber - minNumber + 1) + minNumber);
 };
 
-const checkLength = (string, maxLength) => string.length <= maxLength;
+const addComments = () => ({
+  id: 135,
+  avatar: `img/avatar${randomiseNumber(1, 6)}.svg`,
+  message: 'Всё отлично!',
+  name: NAMES[randomiseNumber(0, NAMES.length - 1)]
+});
 
-randomiseNumber(1, 10);
-checkLength('', 0);
+let id = 0;
 
+const addPhoto = () => {
+  id++;
+  return {
+    id: id,
+    url: `photos/${id}.jpg`,
+    description: 'В целом всё неплохо. Но не всё.',
+    likes: randomiseNumber(Like.MIN, Like.MAX),
+    comments: addComments()
+  };
+};
+
+const photos = Array.from({length: COUNT}, addPhoto);
+export {photos};
